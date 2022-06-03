b5951ac1cb708ae724556337572dfb892fa9d71e
SutodaIbraham <Sutoda.Ibraham@schabmueller.de>
2022-06-03 09:59:21 +0200

    Update Impreagnieranlge2.py

--- Impreagnieranlge2.py
+++ Impreagnieranlge2.py
@@ -8,37 +8,35 @@ import glob
 import pandas as pd
 import pandas.io.sql
 import pyodbc
-
-filenames = glob.glob("I:\Arbeitsgruppen\Prozessdatendokumentation\PM_Fertigung\TSS-214\Meier_Impraegnieranlage\Datensicherung MMC HMI Panel_08.04.2021\SAVELOG/*.csv")
-dfs = []
-for filename in filenames:
-    p = pd.read_csv(filename, sep=";" ) 
-    p['Datum']= pd.to_datetime(p['TimeString']).dt.date
-    p['Zeit'] = pd.to_datetime(p['TimeString']).dt.time
-    p['VarValue']=p['VarValue'].str.replace(',','.')
-    p['Time_ms']=p['Time_ms'].str.replace(',','.')
-    p['VarValue'] = pd.to_numeric(p['VarValue'], errors = 'ignore')
-    p['Time_ms'] = pd.to_numeric(p['Time_ms'], errors = 'ignore')
-    dfs.append(p)
-big_frame = pd.concat(dfs, ignore_index=True)
-
+from datetime import datetime
 server = 'ltp077'
 db = 'TraceabilityTest'
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + db + ';Trusted_Connection=yes')
 cursor = conn.cursor()
 
-for row in big_frame.itertuples():
-    try:
+filenames = glob.glob("I:\Arbeitsgruppen\Prozessdatendokumentation\PM_Fertigung\TSS-214\Meier_Impraegnieranlage\Datensicherung MMC HMI Panel_08.04.2021\SAVELOG/*.csv")
+for filename in filenames:
+  try:
+    p = pd.read_csv(filename, sep=";" )
+    
+   
+    p['VarValue']=p['VarValue'].str.replace(',','.')
+    p['Time_ms']=p['Time_ms'].str.replace(',','.')
+    p['VarValue'] = pd.to_numeric(p['VarValue'], errors = 'ignore')
+    p['Time_ms'] = pd.to_numeric(p['Time_ms'], errors = 'ignore')
+    
+    for row in p.itertuples():
       cursor.execute('''
-                INSERT INTO dbo.Impreagnieranlage1 (Varname,TimeString,VarValue,Validity,Time_ms,Date1,Time1)
-                VALUES (?,?,?,?,?,?,?)''',
-                row.VarName, row.TimeString, row.VarValue, row.Validity, row.Time_ms, row.Datum, row.Zeit)
+                INSERT INTO dbo.Impreagnieranalge (Varname,TimeString,VarValue,Validity,Time_ms)
+                VALUES (?,?,?,?,?)''',
+                row.VarName, row.TimeString, row.VarValue, row.Validity, row.Time_ms)
 
       conn.commit()
-    except Exception as Argument:
+  except Exception as Argument:
             filename1 = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
-            f = open("I:\Arbeitsgruppen\Traceability\Fehlerbehandlung\Impreagnieranlage" + filename1+ ".txt", "a")
+            f = open("I:\Arbeitsgruppen\Traceability\Sutoda-Ibraham\Fehlerbehandlung\Impreagnieranlage_" + filename1+ ".txt", "a+")
+            f.write(f'{filename}\n')
             f.write(str(Argument))
-            f.write(f'{row}\n')
+            f.write(f'\n{row}\n')
             f.close()
 conn.close()
